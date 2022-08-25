7c2c2b7dec5fcc9132626d391792f4c05dc1fd68
barbiwonderland <barbiiee.8@gmail.com>
2022-08-25 13:30:27 -0300

    first commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    nickname: { type: String, required: true },
+    age: { type: Number, required: true },
+    // date: { type: Date, required: true },
+
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, }
+);
+const connection = mongoose.connection;
+connection.once('open', () =>
+{
+    console.log("MongoDB database connection established successfully");
+});
+
+const usersRouter = require('./routes/user');
+
+
+app.use('/users', usersRouter);
+
+app.listen(port, () =>
+{
+    console.log(`Server is running on port: ${port}`);
+});
