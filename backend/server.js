0bf7a91c1fe1eb68e9dc1a0fd553faf2908cb674
barbiwonderland <barbiiee.8@gmail.com>
2022-08-27 15:59:24 -0300

    port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
