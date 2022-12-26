09ba5850a97455f4fda63aeba026f6858eca527b
baarbarabottazzi <barbiiee.8@gmail.com>
2022-12-26 18:07:18 -0300

    logica redux

--- frontend/src/components/FormAddOrEditUsers.js
+++ frontend/src/components/FormAddOrEditUsers.js
@@ -1,146 +1,93 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field } from "formik";
-import { addUser, EditUser } from '../services/userService';
-import { useParams } from 'react-router-dom';
-import { Moda_l } from './Modal';
+import React, { useState } from "react"
+import { Formik, Form, Field } from "formik"
+import { addUser, EditUser } from "../services/userService"
+import { useParams } from "react-router-dom"
+import { Moda_l } from "./Modal"
+import { useDispatch, useSelector } from "react-redux"
+import { addUserRedux, editUserRedux } from "../redux/reducers/userSlice"
 
-export const FormAddOrEditUsers = ({ isEdit, updateUser, setUserFiltered, userFiltered, setShowToast, handleClose, show }) =>
-{
+export const FormAddOrEditUsers = ({
+  isEdit,
+  updateUser,
+  setShowToast,
+  handleClose,
+  show,
+}) => {
+  const { id } = useParams()
+  // Messages
+  const required = "This field is required"
+  const maxLength = "Your input exceed maximum length"
+  const dispatch = useDispatch()
+  const { users } = useSelector((state) => state.users)
+  if (isEdit) {
+    var actualUser = users.filter((user) => user._id == id)
+    actualUser = actualUser[0]
+  }
+  return (
+    <>
+      <Moda_l show={show} handleClose={handleClose} setShowToast={setShowToast}>
+        <Formik
+          initialValues={
+            isEdit
+              ? {
+                  email: actualUser.email,
+                  name: actualUser.name,
+                  age: actualUser.age,
+                }
+              : { email: "", name: "", age: "" }
+          }
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500))
+            if (!isEdit) {
+              console.log(values, "valores a agregar")
+              addUser(values).then((res) => console.log(res.data, "hjhajahaka"))
+              dispatch(addUserRedux(values))
+              setShowToast(true)
+              handleClose()
+            } else {
+              EditUser(values, id)
+              dispatch(editUserRedux({ user: values, id: id }))
+              setShowToast(true)
+              handleClose()
+            }
+          }}
+          validator={() => ({})}
+        >
+          <div className="container">
+            <div className=" input-group mb-3 d-flex  justify-content-center">
+              <Form className="w-75 ">
+                <Field
+                  type="text"
+                  name="email"
+                  className="form-control "
+                  placeholder="example@example.com"
+                />
 
+                <Field
+                  className="form-control mt-2"
+                  type="text"
+                  placeholder="name"
+                  name="name"
+                />
 
+                <Field
+                  className="form-control  mt-2"
+                  type="number"
+                  placeholder="Age"
+                  name="age"
+                />
 
-    const { id } = useParams();
-    // Messages
-    const required = "This field is required";
-    const maxLength = "Your input exceed maximum length";
-
-    // Error Component
-    const errorMessage = error =>
-    {
-        return <div className="invalid-feedback">{error}</div>;
-    };
-    const validateUserEmail = value =>
-    {
-        let error;
-        if (!value)
-        {
-            error = required;
-        } else if (value.length > 12)
-        {
-            error = maxLength;
-        }
-        return error;
-    };
-    const validatename = value =>
-    {
-        let error;
-        if (!value)
-        {
-            error = required;
-        } else if (value.length > 12)
-        {
-            error = maxLength;
-        }
-        return error;
-    };
-    const validateAge = value =>
-    {
-        let error;
-        if (value.length > 2)
-        {
-            error = maxLength;
-        }
-        return error;
-    };
-
-
-    return (
-        <>
-            <Moda_l show={show} handleClose={handleClose} setShowToast={setShowToast}>
-                <Formik
-
-                    initialValues={isEdit ? {
-                        email: updateUser.email, name: updateUser.name, age: updateUser.age
-                    } : { email: "", name: "", age: "" }
-                    }
-                    onSubmit={(values, { setSubmitting }) =>
-                    {
-                        if (!isEdit)
-                        {
-                            console.log(values, "valores a agregar");
-                            addUser(values).then(res => console.log(res.data, "hjhajahaka"));
-                            setShowToast(true);
-                            handleClose();
-
-                        } else
-                        {
-                            EditUser(values, id);
-                            // creo un array copia del original user traido de "ListUsers"
-                            const usersEdited = userFiltered;
-                            // Encuentro el indice del elemento de usuarios que quiero modificar
-                            const objIndex = usersEdited.findIndex((obj => obj._id == id));
-                            // Modifico directamente el elemento que quiero modificar a través del index
-                            usersEdited[objIndex].email = values.email;
-                            usersEdited[objIndex].name = values.name;
-                            usersEdited[objIndex].age = values.age;
-                            // Modifico el estado user con userEdited
-                            setUserFiltered(usersEdited);
-                            setShowToast(true);
-                            handleClose();
-
-                        }
-
-                        setTimeout(() =>
-                        {
-                            console.log(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
-                    }}
+                <button
+                  className="btn btn-dark mt-2 d-flex mx-auto  "
+                  type="submit"
                 >
-                    {({ errors, touched, isValidating }) => (
-                        <div className="container">
-                            <div className=" input-group mb-3 d-flex  justify-content-center">
-                                <Form className='w-75 '>
-                                    <Field
-                                        type="text"
-                                        name="email"
-                                        className="form-control "
-                                        placeholder="example@example.com"
-                                        validate={validateUserEmail}
-                                    />
-                                    {errors.email && touched.email && errorMessage(errors.email)}
-
-                                    <Field
-                                        className="form-control mt-2"
-                                        type="text"
-                                        placeholder="name"
-                                        name="name"
-                                        validate={validatename}
-                                    />
-                                    {errors.name && touched.name && errorMessage(errors.name)}
-                                    <Field
-                                        className="form-control  mt-2"
-                                        type="number"
-                                        placeholder="Age"
-                                        name="age"
-                                        validate={validateAge}
-                                    />
-                                    {errors.age &&
-                                        touched.age &&
-                                        errorMessage(errors.age)}
-
-
-                                    <button className="btn btn-dark mt-2 d-flex mx-auto  " type="submit" >
-                                        {isEdit ? "Editar" : "Agregar"}
-
-                                    </button>
-
-                                </Form>
-                            </div>
-                        </div>
-                    )}
-                </Formik>
-            </Moda_l>
-        </>
-    );
-};
+                  {isEdit ? "Editar" : "Agregar"}
+                </button>
+              </Form>
+            </div>
+          </div>
+        </Formik>
+      </Moda_l>
+    </>
+  )
+}
--- frontend/src/redux/reducers/userSlice.js
+++ frontend/src/redux/reducers/userSlice.js
@@ -1,55 +1,56 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getUsers } from '../../services/userService';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { getUsers } from "../../services/userService"
 const initialState = {
   loading: false,
   users: [],
   error: "",
-};
-export const fetchUsers = createAsyncThunk("user/fetchUsers", () =>
-{
-  return getUsers().then(response => response.data);
-});
+}
+export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return getUsers().then((response) => response.data)
+})
 export const usersSlice = createSlice({
-  name: 'users',
+  name: "users",
   initialState,
-  extraReducers: (builder) =>
-  {
-    builder.addCase(fetchUsers.pending, (state) =>
-    {
-      state.loading = true;
-    });
-    builder.addCase(fetchUsers.fulfilled, (state, action) =>
-    {
-      state.loading = false;
-      state.users = action.payload;
-      state.error = "";
-    });
-    builder.addCase(fetchUsers.rejected, (state, action) =>
-    {
-      state.loading = false;
-      state.users = [];
-      state.error = action.error.message;
-    });
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.loading = false
+      state.users = action.payload
+      state.error = ""
+    })
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.loading = false
+      state.users = []
+      state.error = action.error.message
+    })
   },
   reducers: {
-
-    addUser: (state, action) =>
-    {
-      state.users = [...state, action.payload];
+    addUserRedux: (state, action) => {
+      state.users = [...state.users, action.payload]
     },
-    editUser: (state, action) =>
-    {
-      state.value += action.payload;
+    editUserRedux: (state, action) => {
+      state.users = state.users.map((user) => {
+        console.log(user)
+        if (user._id === action.payload.id) {
+          // todo lo que esta en user y todo lo que me pasaron por param del nuevo usser(se modifican solo los campos que se mandan nuevosd y el resto queda igual)
+          return (user = { ...user, ...action.payload.user })
+        } else {
+          return user
+        }
+      })
     },
-    deleteUser: (state, action) =>
-    {
-      const newList = state.filter(user => user.id != action.payload.id);
-      state.users = newList;
+    deleteUserRedux: (state, action) => {
+      const id = action.payload
+      console.log(id)
+      state.users = state.users.filter((user) => user._id != id)
     },
   },
-});
+})
 
 // Action creators are generated for each case reducer function
-export const { addUser, editUser, deleteUser } = usersSlice.actions;
+export const { addUserRedux, editUserRedux, deleteUserRedux } =
+  usersSlice.actions
 
-export default usersSlice.reducer;
+export default usersSlice.reducer
